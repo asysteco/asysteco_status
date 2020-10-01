e39578dd3fd4f9b51c1e1d34fdc48157048af49f
asysteco <admin@asysteco.com>
2020-10-01 16:35:07 +0200

    MDB initial skeleton

---
+++ view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <link rel="stylesheet" href="MDB/css/bootstrap.min.css">
+    <link rel="stylesheet" href="MDB/css/mdb.min.css">
+    <link rel="stylesheet" href="MDB/css/style.css">
+    <title>System Status</title>
+</head>
+
+<body>
+    <script type="text/javascript" src="MDB/js/jquery.min.js"></script>
+    <script type="text/javascript" src="MDB/js/popper.min.js"></script>
+    <script type="text/javascript" src="MDB/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="MDB/js/mdb.min.js"></script>
+    <div class="container-fluid" style="text-align:center; margin-top: 25px;">
+        <h2>Uso de bases de datos</h2>
+        <canvas id="total-profesores" style="max-width: 500px; margin-left:auto; margin-right: auto; display:inline-block;"></canvas>
+        <canvas id="total-fichajes" style="max-width: 500px; margin-left:auto; margin-right: auto; display:inline-block;"></canvas>
+        <canvas id="fichajes-diarios" style="max-width: 500px; margin-left:auto; margin-right: auto; display:inline-block;"></canvas>
+    </div>
+</body>
+
+</html>
